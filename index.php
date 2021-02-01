1c870c5f7155cad3f15f7530e64b35d954cbd2bd
onyweb <onyweb.contact@gmail.com>
2021-02-01 20:00:18 +0100

    Problem to create a user

--- index.php
+++ index.php
@@ -9,7 +9,29 @@ $factory = (new Factory)->withServiceAccount('secret/test-cb42f-f9d8f278b806.jso
 $firestore = $factory->createFirestore();
 $database = $firestore->database();
 
-$ref = $database->collection('messages');
-$snapshot = $ref->documents();
+use Kreait\Firebase\Auth;
 
-var_dump($snapshot);
+//$auth = (new Auth)-> ??????????
+
+$userProperties = [
+    'email' => 'user@example.com',
+    'emailVerified' => false,
+    'phoneNumber' => '+15555550100',
+    'password' => 'secretPassword',
+    'displayName' => 'John Doe',
+    'photoUrl' => 'http://www.example.com/12345678/photo.png',
+    'disabled' => false,
+];
+
+$createdUser = $auth->createUser($userProperties);
+
+// This is equivalent to:
+
+$request = \Kreait\Auth\Request\CreateUser::new()
+    ->withUnverifiedEmail('user@example.com')
+    ->withPhoneNumber('+15555550100')
+    ->withClearTextPassword('secretPassword')
+    ->withDisplayName('John Doe')
+    ->withPhotoUrl('http://www.example.com/12345678/photo.png');
+
+$createdUser = $auth->createUser($request);
